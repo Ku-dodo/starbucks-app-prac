ac90174d97f8222bac1f098e62c7ca95d28c210d
dohyun ku <rnehgus9403@gmail.com>
2023-08-15 22:40:44 +0900

    add promotion section

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const searchEl = document.querySelector('.search');
 const searchInputEl = searchEl.querySelector('input');
 
 searchEl.addEventListener('click', function () {
-  searchInputEl.focus();  
+  searchInputEl.focus();
 });
 
 searchInputEl.addEventListener('focus', function () {
@@ -18,7 +18,7 @@ searchInputEl.addEventListener('blur', function () {
 
 const badgeEl = document.querySelector('header .badges');
 
-window.addEventListener('scroll', _.throttle(function() {
+window.addEventListener('scroll', _.throttle(function () {
   // _.throttle(함수, 시간)
   console.log(window.scrollY);
   if (window.scrollY > 500) {
@@ -36,3 +36,51 @@ window.addEventListener('scroll', _.throttle(function() {
     });
   }
 }, 300));
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7,
+    opacity: 1
+  });
+});
+
+// 
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical',
+  autoplay: true,
+  loop: true
+});
+
+new Swiper('.promotion .swiper-container', {
+  direction: 'horizontal',
+  slidesPerView: 3,
+  spaceBetween: 10,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 5000
+  },
+  pagination: {
+    el: '.promotion .swiper-pagination', //페이지
+    clickable: true, //사용자의 페이지 번호 요소 제어 가능 여부
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+})
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function () {
+  isHidePromotion = !isHidePromotion
+  if (isHidePromotion) {
+    //숨김
+    promotionEl.classList.add('hide');
+  } else {
+    //보임
+    promotionEl.classList.remove('hide');
+  }
+})
